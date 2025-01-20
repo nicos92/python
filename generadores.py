71dedd84225c7353268ccb5441cca4b17be3acf3
nicos92 <nicolassandoval140692@gmail.com>
2025-01-20 13:05:50 -0300

    COMENZANDO A VER GENERADORES

---
+++ generadores.py
@@ -0,0 +1,10 @@
+# GENERADORES
+
+"""
+Generadores ¿Qué son?
+• Estructuras que extraen valores de una función y se almacenan en objetos iterables (que se
+pueden recorrer).
+• Estos valores se almacenan de uno en uno.
+• Cada vez que un generador almacena un valor, esta permanece en un estado pausado hasta que
+se solicita el siguiente. Esta característica es conocida como "suspensión de estado".
+"""
