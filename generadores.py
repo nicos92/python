5378bb6d264934316c8b3aad117324472a4d8d7a
nicos92 <nicolassandoval140692@gmail.com>
2025-01-20 17:02:54 -0300

    comenzando con excepciones

--- generadores.py
+++ generadores.py
@@ -7,4 +7,78 @@ pueden recorrer).
 • Estos valores se almacenan de uno en uno.
 • Cada vez que un generador almacena un valor, esta permanece en un estado pausado hasta que
 se solicita el siguiente. Esta característica es conocida como "suspensión de estado".
+
+| Generadores ¿Qué utilidad tienen?
+* Son más eficientes que las funciones tradicionales
++ Muy útiles con listas de valores infinitos
+* Bajo determinados escenarios, será muy útil que un generador devuelva los valores de uno en uno.
+* adicionalmente puede utilizar un return
 """
+
+
+def generadorPares(limite):
+    num = 1
+    while num < limite:
+        yield num * 2
+        num += 1
+
+
+guardaPares = generadorPares(8)
+
+# * aprendiendo a usar generadores
+
+
+# adv el bucle for no es una buena forma de usar el generador
+"""
+#! no poner un for antes por que sino el next ya no cumple su funcion
+for i in guardaPares:
+    print("lista: ", i)
+"""
+# mejor usar el metodo next
+
+print(next(guardaPares))
+
+print("Lineas de codigo al azar")
+
+print(next(guardaPares))
+
+print("mas lineas de codigo al azar")
+
+
+# * Yield from
+
+# * Utilidad: Simplificar el código de los generadores en el caso de utilizar bucles anidados
+
+
+def devuelveCiudades(*ciudades):
+    """# adv "*ciudades" puede recibiar 1 o varios elementos, numero indeterminado de elementos. los va reciir en forma de tupla
+
+    Yields:
+        _type_: _description_
+    """
+    for elemto in ciudades:
+        for subElem in elemto:
+            yield subElem
+
+
+guardaCiudades = devuelveCiudades("Buenos aires", "Cordoba", "Mendoza", "La Pampa")
+
+print(next(guardaCiudades))
+print(next(guardaCiudades))
+
+# * Yield from
+
+
+# * Utilidad: Simplificar el código de los generadores en el caso de utilizar bucles anidados
+# * usando yields from
+# adv "*ciudades" puede recibiar 1 o varios elementos, numero indeterminado de elementos. los va reciir en forma de tupla
+def devuelveCiudadess(*ciudades):
+    for elemto in ciudades:
+        yield from elemto
+
+
+guardaCiudadess = devuelveCiudadess("Buenos aires", "Cordoba", "Mendoza", "La Pampa")
+
+
+print(next(guardaCiudadess))
+print(next(guardaCiudadess))
