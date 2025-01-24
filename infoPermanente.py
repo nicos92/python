d3a85d3eb15efd00e9921cc93b5994369824edc7
nicos92 <nicolassandoval140692@gmail.com>
2025-01-24 06:21:01 -0300

    guardado en archivos binarios

--- infoPermanente.py
+++ infoPermanente.py
@@ -1,67 +1,69 @@
 import pickle
 
+
 class Persona:
 
     def __init__(self, nombre, genero, edad):
         self.nombre = nombre
-        self.genero= genero
+        self.genero = genero
         self.edad = edad
-        print("Se creo una persona con el nombre " , self. nombre)
+        print("Se creo una persona con el nombre ", self.nombre)
 
     def __str__(self):
         return "{} {} {}".format(self.nombre, self.genero, self.edad)
 
-class ListaPersonas:
 
-    personas =[]
+class FicheroPersonas:
 
-    def guardarPersonas(self, p):
-        self.personas.append(p)
+    personas = []
 
-
-    def verPersonas(self):
-        for p in self.personas:
-            print(p)
-
-class MiFichero:
-    def __init__(self, lista_personas):
+    def __init__(self):
         self.__fichero = "ficheroPersonas"
-        
+
         self.ficheroPersonas = open(self.__fichero, "ab+")
         self.ficheroPersonas.seek(0)
-        self.verificarCargaPersonas(lista_personas)
+        self.verificarCargaPersonas()
 
-    def verificarCargaPersonas(self, lista_personas):
+    def verificarCargaPersonas(self):
         try:
-            lista_personas = pickle.load(self.ficheroPersonas)
-            print(len(lista_personas))
+            self.personas = pickle.load(self.ficheroPersonas)
+            print(len(self.personas))
 
         except:
             print("El fichero '{}' esta vacio".format(self.__fichero))
         finally:
             self.ficheroPersonas.close()
-            del(self.ficheroPersonas) 
+            del self.ficheroPersonas
+
+    def guardarPersonas(self, p):
+        self.personas.append(p)
+        self.guardarPersonasFicheroBinario()
+
+    def verPersonas(self):
+        print("La info del fichero binario es: ")
+        for p in self.personas:
+            print(p)
 
     def guardarPersonasFicheroBinario(self):
         self.ficheroPersonas = open(self.__fichero, "wb")
-        pickle.dump(lista_personas, self.ficheroPersonas)
+        pickle.dump(self.personas, self.ficheroPersonas)
         self.ficheroPersonas.close()
-        del(self.ficheroPersonas)
+        del self.ficheroPersonas
 
 
-lista_personas = ListaPersonas()
-MiFichero(lista_personas)
-
-'''
+miLista = FicheroPersonas()
 
 p1 = Persona("FRAN", "M", 32)
 p2 = Persona("MARIA", "F", 33)
 p3 = Persona("NICO", "M", 34)
 p4 = Persona("ANA", "F", 35)
-lista_persona.guardarPersonas(p1)
-lista_persona.guardarPersonas(p2)
-lista_persona.guardarPersonas(p3)
-lista_persona.guardarPersonas(p4)
+miLista.guardarPersonas(p2)
+miLista.guardarPersonas(p3)
+miLista.guardarPersonas(p4)
 
-lista_persona.verPersonas()
-'''
+miLista.guardarPersonas(p1)
+miLista.verPersonas()
+
+
+
+
